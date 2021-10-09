83bd8ac626ec0a35d88b7202c606f817b163f206
kalildeoliveira <69366429+kalildeoliveira@users.noreply.github.com>
2021-10-09 18:22:05 -0300

    Add files via upload

---
+++ atividade-pratica-03/pinturaCone.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class pinturaCone {
+
+	/**
+	 * lê altura, raio e tipo de tinta e retorna a área, litros, latas e valor total com
+	 * 
+	 * 		a) valor da lata
+	 * 			tipo 1) R$ 238,9
+	 * 			tipo 2) R$ 467,98
+	 * 			tipo 3) R$ 758,34
+	 * 
+	 * 		b) capacidade da lata
+	 * 			volume: 18 litros
+	 * 			rendimento: 3,45 litros/m² 
+	 */
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		double altura=0, raio=0, area=0, litros=0, latas=0, valorTotal=0;
+		String opcao="";
+		
+		System.out.println("Informe a altura e o raio do cone.");
+		System.out.print("Altura: ");
+		altura = entrada.nextDouble();
+	}
+
+}
